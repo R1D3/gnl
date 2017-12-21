de8800db3f291716805e1676092b0035157545e6
R1 <R1@MacBook-Pro-de-Thibault.local>
2017-12-21 13:52:44 +0100

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -1,73 +1,37 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: emassou <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/06 10:38:21 by emassou           #+#    #+#             */
-/*   Updated: 2017/12/06 10:38:22 by emassou          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 
-
-int 	ft_saveline(char **buf, char **line)
+int     nlen(char *str)
 {
-	int size;
-	while(**buf != '\0')
-	{
-		if(ft_strchr(*buf, '\n'))
-		{
-			size = ft_strchr(*buf, '\n') - *buf;
-			*line = ft_strsub(*buf, 0, size);
-			*buf = ft_strchr(*buf, '\n') + 1;
-			return(1);
-		}
-		*line = ft_strdup(*buf);
-		return(1);
-	}
-	return(0);
+    int i;
+    i = 0;
+    if (str == NULL)
+        return (0);
+    while (str[i] != '\n' && str[i] != '\0')
+        i++;
+    return (i);
 }
 
-int 	ft_read(int ret, int fd, char **buf, char **line)
+int     get_next_line(const int fd, char **line)
 {
-	char	*tmp;
-	int 	size;
+    static t_struct p;
+    int             ret;
 
-		tmp = ft_strnew(BUFF_SIZE);
-		while ((ret = read(fd, tmp, BUFF_SIZE)))
-		{
-			tmp[ret] = '\0';
-			*buf = ft_strjoin(*buf,tmp);
-		}
-		free(tmp);
-		if(ft_strchr(*buf, '\n'))
-		{
-			size = ft_strchr(*buf, '\n') - *buf;
-			*line = ft_strsub(*buf,0, size);
-			*buf = ft_strchr(*buf, '\n') + 1;
-		}
-	return (1);
-}
-
-int		get_next_line(const int fd, char **line)
-{
-	static char	*buf;
-	int ret;
-	
-	if(!fd || !*line)
-		return (-1);
-	if(buf)
-		if(ft_saveline(&buf, &*line) == 0) 
-			return (1);
-	if(!buf)
-	{
-		buf = ft_strnew(BUFF_SIZE);
-		if(ft_read(ret, fd, &buf, &*line) == -1)
-			return (-1);
-		return (1);
-	}
-	return (0);
+    if (!p.stock[fd])
+        p.stock[fd] = ft_strnew(BUFF_SIZE);
+    while (!(ft_strchr(p.stock[fd], '\n')) && (ret = read(fd, p.buf, BUFF_SIZE)))
+    {
+        if (ret == -1)
+            return (-1);
+        p.buf[ret] = '\0';
+        p.tmp = ft_strjoin(p.stock[fd], p.buf);
+        ft_strdel(&p.stock[fd]);
+        p.stock[fd] = p.tmp;
+    }
+    if (ft_strlen(p.stock[fd]) == 0)
+        return (0);   
+    *line = ft_strsub(p.stock[fd], 0, nlen(p.stock[fd]));
+    p.tmp2 = ft_strsub(p.stock[fd], nlen(p.stock[fd]) + 1, ft_strlen(&p.stock[fd][nlen(p.stock[fd])]));
+    ft_strdel(&p.stock[fd]);
+    p.stock[fd] = p.tmp2;
+    return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,29 +1,21 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: emassou <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/06 10:38:29 by emassou           #+#    #+#             */
-/*   Updated: 2017/12/06 10:38:31 by emassou          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 2
+# include "libft.h"
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <unistd.h>
+	
+#define BUFF_SIZE  4096
 
-#include "libft/libft.h"
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
-
+typedef struct	s_struct
+{
+	char		*stock[BUFF_SIZE + 1];
+	char		buf[BUFF_SIZE + 1];
+	char		*tmp;
+    char        *tmp2;
+}               t_struct;
 int get_next_line(const int fd, char **line);
-
+	
 #endif
