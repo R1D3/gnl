7b029042bb75dd0b648db32fe4d9404a357dba78
Erwan MASSOU <emassou@e3r3p10.42.fr>
2018-01-05 19:16:04 +0100

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -1,37 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emassou <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/05 19:08:49 by emassou           #+#    #+#             */
+/*   Updated: 2018/01/05 19:08:51 by emassou          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-int     nlen(char *str)
+int	nlen(char *str)
 {
-    int i;
-    i = 0;
-    if (str == NULL)
-        return (0);
-    while (str[i] != '\n' && str[i] != '\0')
-        i++;
-    return (i);
+	int i;
+
+	i = 0;
+	if (str == NULL)
+		return (0);
+	while (str[i] != '\n' && str[i] != '\0')
+		i++;
+	return (i);
 }
 
-int     get_next_line(const int fd, char **line)
+int	get_next_line(const int fd, char **line)
 {
-    static t_struct p;
-    int             ret;
+	static char		*stock[BUFF_SIZE + 1];
+	t_struct		p;
+	int				ret;
 
-    if (!p.stock[fd])
-        p.stock[fd] = ft_strnew(BUFF_SIZE);
-    while (!(ft_strchr(p.stock[fd], '\n')) && (ret = read(fd, p.buf, BUFF_SIZE)))
-    {
-        if (ret == -1)
-            return (-1);
-        p.buf[ret] = '\0';
-        p.tmp = ft_strjoin(p.stock[fd], p.buf);
-        ft_strdel(&p.stock[fd]);
-        p.stock[fd] = p.tmp;
-    }
-    if (ft_strlen(p.stock[fd]) == 0)
-        return (0);   
-    *line = ft_strsub(p.stock[fd], 0, nlen(p.stock[fd]));
-    p.tmp2 = ft_strsub(p.stock[fd], nlen(p.stock[fd]) + 1, ft_strlen(&p.stock[fd][nlen(p.stock[fd])]));
-    ft_strdel(&p.stock[fd]);
-    p.stock[fd] = p.tmp2;
-    return (1);
+	if (fd < 0)
+		return (-1);
+	if (!stock[fd])
+		stock[fd] = ft_strnew(BUFF_SIZE);
+	while (!(ft_strchr(stock[fd], '\n')) && (ret = read(fd, p.b, BUFF_SIZE)))
+	{
+		if (ret == -1)
+			return (-1);
+		p.b[ret] = '\0';
+		p.tmp = ft_strjoin(stock[fd], p.b);
+		ft_strdel(&stock[fd]);
+		stock[fd] = p.tmp;
+	}
+	if (ft_strlen(stock[fd]) == 0)
+		return (0);
+	*line = ft_strsub(stock[fd], 0, nlen(stock[fd]));
+	p.tmp2 = ft_strsub(stock[fd], nlen(stock[fd]) + 1,
+	ft_strlen(&stock[fd][nlen(stock[fd])]));
+	ft_strdel(&stock[fd]);
+	stock[fd] = p.tmp2;
+	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,21 +1,31 @@
-#ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emassou <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/05 19:09:03 by emassou           #+#    #+#             */
+/*   Updated: 2018/01/05 19:09:05 by emassou          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-# include "libft.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
 # include <fcntl.h>
 # include <sys/stat.h>
 # include <sys/types.h>
 # include <unistd.h>
-	
-#define BUFF_SIZE  4096
+# define BUFF_SIZE  4
 
 typedef struct	s_struct
 {
-	char		*stock[BUFF_SIZE + 1];
-	char		buf[BUFF_SIZE + 1];
 	char		*tmp;
-    char        *tmp2;
-}               t_struct;
-int get_next_line(const int fd, char **line);
-	
+	char		*tmp2;
+	char		b[BUFF_SIZE + 1];
+}				t_struct;
+
+int				get_next_line(const int fd, char **line);
 #endif
